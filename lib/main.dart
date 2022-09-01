846e027354d0192526825b7709782617f97f6475
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-01 10:33:05 +0500

    Added routes and multi-providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
 import './providers/products_provider.dart';
-import 'package:provider/provider.dart';
+import './providers/cart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,21 +16,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),
-      child: MaterialApp(
-        title: 'EcomHub',
-        theme: ThemeData(
-            fontFamily: 'Lato',
-            backgroundColor: Colors.white,
-            colorScheme: ColorScheme.fromSwatch().copyWith(
-                primary: Colors.black,
-                secondary: const Color.fromARGB(255, 187, 255, 0))),
-        home: const ProductOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen()
-        },
-      ),
-    );
+    return MultiProvider(
+        providers: [
+          Provider(create: (ctx) => ProductsProvider()),
+          Provider(create: (ctx) => Cart()),
+        ],
+        child: MaterialApp(
+          title: 'EcomHub',
+          theme: ThemeData(
+              fontFamily: 'Lato',
+              backgroundColor: Colors.white,
+              colorScheme: ColorScheme.fromSwatch().copyWith(
+                  primary: Colors.black,
+                  secondary: const Color.fromARGB(255, 187, 255, 0))),
+          home: const ProductOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => const CartScreen(),
+          },
+        ));
   }
 }
