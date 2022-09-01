69df7b0821185dc991df83ea0b172e44e49bcc3a
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-01 15:27:35 +0500

    Added order provider with orderItem model

---
+++ lib/providers/order.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+
+import '../providers/cart.dart';
+
+class OrderItem {
+  final String id;
+  final List<CartItem> products;
+  final double amount;
+  final DateTime dateTime;
+
+  OrderItem(
+      {required this.id,
+      required this.amount,
+      required this.products,
+      required this.dateTime});
+}
+
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [...orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+        0,
+        OrderItem(
+            id: DateTime.now().toString(),
+            dateTime: DateTime.now(),
+            amount: total,
+            products: cartProducts));
+    notifyListeners();
+  }
+}
