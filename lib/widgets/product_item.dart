8b742d62bde9806bcd88cdd3841b3c4ba24c1278
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-01 10:31:35 +0500

    Added named routes

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -14,6 +15,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Product product = Provider.of<Product>(context, listen: false);
+    final Cart cart = Provider.of<Cart>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -36,7 +38,9 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
               icon: const Icon(Icons.shopping_cart),
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.title, product.price);
+              },
               color: Theme.of(context).colorScheme.secondary),
         ),
         child: GestureDetector(
