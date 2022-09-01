378fb90b0fc6c34f326bea5bd7b532f4bc2dad82
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-01 08:33:24 +0500

    Added state management and its routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+import './providers/products_provider.dart';
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,12 +14,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'EcomHub',
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            backgroundColor: Colors.white,
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+                primary: Colors.black,
+                secondary: const Color.fromARGB(255, 187, 255, 0))),
+        home: const ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen()
+        },
       ),
-      home: Text('HomePage'),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  const ProductItem(
-      {Key? key, required this.id, required this.title, required this.imageUrl})
-      : super(key: key);
+  const ProductItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        leading: IconButton(icon: const Icon(Icons.favorite), onPressed: () {}),
-        backgroundColor: Colors.black54,
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+    final Product product = Provider.of<Product>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+                color: Theme.of(context).colorScheme.primary),
+          ),
+          backgroundColor: Colors.black54,
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary),
         ),
-        trailing: IconButton(
-          icon: const Icon(Icons.shopping_cart),
-          onPressed: () {},
-        ),
+        child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(product.imageUrl, fit: BoxFit.cover)),
       ),
-      child: Image.network(imageUrl, fit: BoxFit.cover),
     );
   }
 }
