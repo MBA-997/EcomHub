39d9eff7c4b398f832738a3d00e1b0f04b574a5b
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-02 16:02:12 +0500

    Updated user product screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './edit_product_screen.dart';
 import '../providers/products_provider.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
@@ -18,7 +19,21 @@ class UserProductScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Products'),
         actions: <Widget>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
+              icon: const Icon(Icons.add)),
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
+              icon: const Icon(Icons.edit)),
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName);
+              },
+              icon: const Icon(Icons.delete))
         ],
       ),
       drawer: const AppDrawer(),
