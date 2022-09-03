4ae4a2e67172a631cef2982b62ba233a78915356
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-03 20:09:01 +0500

    Added edit and delete button

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products_provider.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  const UserProductItem({Key? key, required this.title, required this.imageUrl})
+  const UserProductItem(
+      {Key? key, required this.id, required this.title, required this.imageUrl})
       : super(key: key);
 
   @override
@@ -17,12 +23,18 @@ class UserProductItem extends StatelessWidget {
         child: Row(
           children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               icon: const Icon(Icons.edit),
               color: Theme.of(context).colorScheme.primary,
             ),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Provider.of<ProductsProvider>(context, listen: false)
+                      .deleteProduct(id);
+                },
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor),
           ],
