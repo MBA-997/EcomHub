ff963fe5db5fa85e5d853d3ed1c47430aaf851ae
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-05 15:33:41 +0500

    Made stateless and added future builder in orders_screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -12,16 +12,32 @@ class OrderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(title: const Text('Your Orders')),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-          itemCount: orderData.orders.length,
-          itemBuilder: (ctx, i) => ord.OrderItem(
-                order: orderData.orders[i],
-              )),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text(
+                    'An error occured. It\'s not you it\'s us. We are looking into it.'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => ord.OrderItem(
+                          order: orderData.orders[i],
+                        )),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
