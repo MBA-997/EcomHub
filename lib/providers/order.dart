0f29f722e7fb6b81dee7df2e8c64d1c22254ddaf
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-05 15:34:46 +0500

    Added orders getter and adder from firebase integration

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 import '../providers/cart.dart';
+import '../models/http_exception.dart';
+
+const API = 'ecomhub-a440c-default-rtdb.firebaseio.com';
 
 class OrderItem {
   final String id;
@@ -22,14 +28,76 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-        0,
-        OrderItem(
-            id: DateTime.now().toString(),
-            dateTime: DateTime.now(),
-            amount: total,
-            products: cartProducts));
-    notifyListeners();
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.https(API, '/orders.json');
+    final List<OrderItem> loadedOrders = [];
+
+    http.get(url).then((response) {
+      if (response.statusCode >= 400) {
+        throw HttpException('Could not load orders.');
+      }
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
+      extractedData.forEach((orderId, orderData) {
+        loadedOrders.add(
+          OrderItem(
+            id: orderId,
+            amount: orderData['amount'],
+            dateTime: DateTime.parse(orderData['dateTime']),
+            products: (orderData['products'] as List<dynamic>)
+                .map(
+                  (item) => CartItem(
+                      id: item['id'],
+                      title: item['title'],
+                      price: item['price'],
+                      quantity: item['quantity']),
+                )
+                .toList(),
+          ),
+        );
+      });
+      _orders = loadedOrders.reversed.toList();
+      notifyListeners();
+    });
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.https(API, '/orders.json');
+    final timestamp = DateTime.now();
+
+    return http
+        .post(
+      url,
+      body: json.encode({
+        'dateTime': timestamp.toIso8601String(),
+        'amount': total,
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'price': cp.price,
+                  'quantity': cp.quantity
+                })
+            .toList()
+      }),
+    )
+        .then((response) {
+      if (response.statusCode >= 400) {
+        throw HttpException('Could not set Favorite.');
+      }
+
+      _orders.insert(
+          0,
+          OrderItem(
+              id: json.decode(response.body)['name'],
+              dateTime: timestamp,
+              amount: total,
+              products: cartProducts));
+      notifyListeners();
+    }).catchError((error) {
+      throw error;
+    });
   }
 }
