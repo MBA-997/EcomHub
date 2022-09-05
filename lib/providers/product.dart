b2801da02cf591291c8f380c709898b64b8a13bf
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-05 15:36:12 +0500

    Added isFavorite patcher with Firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+const API = 'ecomhub-a440c-default-rtdb.firebaseio.com';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +22,33 @@ class Product with ChangeNotifier {
       required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = Uri.https(API, '/products/$id');
+
+    return http
+        .patch(
+      url,
+      body: json.encode({'isFavorite': isFavorite}),
+    )
+        .then((response) {
+      if (response.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+        throw const HttpException('Could not set Favorite.');
+      }
+      isFavorite = !isFavorite;
+
+      notifyListeners();
+    }).catchError((error) {
+      isFavorite = oldStatus;
+      notifyListeners();
+
+      throw error;
+    });
   }
 }
