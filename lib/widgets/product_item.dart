106efb930bb75aec716d36bcf042bcb26c585f7c
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-07 01:26:01 +0500

    Integrated Auth provider for authentication token

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
 import '../providers/cart.dart';
+import '../providers/auth.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -16,6 +17,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final Product product = Provider.of<Product>(context, listen: false);
     final Cart cart = Provider.of<Cart>(context, listen: false);
+    final authData = Provider.of<Auth>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -27,7 +29,7 @@ class ProductItem extends StatelessWidget {
                     ? Icons.favorite
                     : Icons.favorite_border),
                 onPressed: () {
-                  product.toggleFavoriteStatus();
+                  product.toggleFavoriteStatus(authData.token);
                 },
                 color: const Color.fromARGB(255, 187, 255, 0)),
           ),
