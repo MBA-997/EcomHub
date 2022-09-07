83ce545d8386d33ce42f3eda58ffd32c12355fee
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-07 15:48:22 +0500

    Added logout button and integrated auto logout

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
 
 import '../screens/order_screen.dart';
 import '../screens/user_products_screen.dart';
@@ -35,6 +38,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductScreen.routeName);
+            }),
+        const Divider(),
+        ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pushReplacementNamed('/');
+              Provider.of<Auth>(context, listen: false).logout();
             }),
       ]),
     );
