c0e7c1a6180c9e306863eba9cfebef844a459775
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-08 08:04:01 +0500

    Integrated custom transition to product orders

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../helpers/custom_route.dart';
 import '../providers/auth.dart';
 
 import '../screens/order_screen.dart';
@@ -29,7 +30,10 @@ class AppDrawer extends StatelessWidget {
             leading: const Icon(Icons.shop),
             title: const Text('Orders'),
             onTap: () {
-              Navigator.of(context).pushNamed(OrderScreen.routeName);
+              // Navigator.of(context).pushNamed(OrderScreen.routeName);
+              Navigator.of(context).pushReplacement(CustomRoute(
+                builder: (ctx) => const OrderScreen(),
+              ));
             }),
         const Divider(),
         ListTile(
