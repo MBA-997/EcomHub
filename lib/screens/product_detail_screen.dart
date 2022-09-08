a77e9e43b074072a2b382ecaf0af16b32bd1d475
S M Bilal Arshad <syedmuhammadbilalarshad@gmail.com>
2022-09-08 08:01:47 +0500

    Added dynamic appbar and smooth transition to product detail

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -17,36 +17,45 @@ class ProductDetailScreen extends StatelessWidget {
         .findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 200,
-              width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
             const SizedBox(height: 10),
             Text(
               '\$${loadedProduct.price}',
               style: const TextStyle(fontSize: 20, color: Colors.grey),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 10),
             Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                width: double.infinity,
-                child: Text(
-                  loadedProduct.description,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ))
-          ],
-        ),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ]))
+        ],
       ),
     );
   }
